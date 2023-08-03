fb41ec8c5ccdc6432c36048ddb4956d373acdbe0
samsil arefin <samsilarefin017@gmail.com>
2023-08-03 04:22:45 +0200

    uodate input field and create style.css

--- src/components/InpurtField.tsx
+++ src/components/InpurtField.tsx
@@ -1,1 +1,15 @@
 import React from 'react';
+import "./styles.css";
+
+
+const InputField =()=>{
+    return(
+        <form className='input'>
+
+            <input type='input' placeholder='Enter your task' className='inputbox'/>
+            <button type='submit' className='input_submit'>Add</button>
+
+        </form>
+    )
+}
+export default InputField
