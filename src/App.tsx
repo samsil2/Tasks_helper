104f318813ad013fcef639bcd5502efa4e86a50e
samsil arefin <samsilarefin017@gmail.com>
2023-10-26 13:12:13 +0200

    create singtodo component file

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,14 @@ const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>("");
   const [todos,setTodos] = useState<Todo[]>([]);
+ 
 
   const handleAdd = (e: React.FormEvent) =>{
     e.preventDefault();
 
     if (todo) {
       setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
-      setTodo("");
+      setTodo(""); //make the form empty after submiting
     }
 
   }
@@ -26,7 +27,8 @@ const App: React.FC = () => {
       <span className='heading'> Task Helper</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
 
-      {<TodoList/>}
+      {/* {<TodoList/>} */}
+      <TodoList todos={todos} setTodos={setTodos}  />
     
 
     </div>
